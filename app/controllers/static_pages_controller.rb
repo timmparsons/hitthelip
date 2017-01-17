615097ef6820aebe77ea0e0c142021ea3ec63306
Tim Parsons <tim.m.parsons@hotmail.com>
2017-01-17 11:33:48 -0800

    5.3 revisions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,10 +11,7 @@ class StaticPagesController < ApplicationController
 	@name = parmas[:name]
 	@email = params[:email]
 	@message = params[:message]
-	ActionMailer::Base.mail(:from => @email,
-		:to => 'tim.m.parsons@hotmail.com',
-		:subject => "A new contact form message from #{@name}",
-		:body => @message).deliver_now
+	UserMailer.contact_form(@email, @name, @message).deliver_now
 end
 
 end
