6bd325d5fe08b0302990a0c39d0cc7206a29e911
Tim Parsons <tim.m.parsons@hotmail.com>
2017-01-17 11:54:23 -0800

    5.3 revisions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,7 +8,7 @@ class StaticPagesController < ApplicationController
 	end
 
 	def thank_you
-	@name = parmas[:name]
+	@name = params[:name]
 	@email = params[:email]
 	@message = params[:message]
 	UserMailer.contact_form(@email, @name, @message).deliver_now
