dffb6033080e4e33ffcb819181ca9612a6a9f257
Tim Parsons <tim.m.parsons@hotmail.com>
2017-01-20 18:33:15 -0800

    exercise 5.8

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,1 +1,5 @@
 $(".alert").delay(200).fadeOut(300);
+
+$(document).on('turbolinks:load', function (){
+	$(',rating').raty ( { path: '/assets', scoreName: 'comment[rating]' });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
-  resources :users
-  resources :products
-  get 'static_pages/contact'
+  
+   get 'static_pages/contact'
 
   get 'static_pages/about'
 
@@ -14,5 +13,9 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
+  resources :products do
+    resources :comments
+  end
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
