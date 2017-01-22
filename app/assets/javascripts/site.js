5411b4ca322d82285d8509db9c7fdc93e9afbd09
Tim Parsons <tim.m.parsons@hotmail.com>
2017-01-21 18:03:12 -0800

    5.8 revisions

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,11 @@
 $(".alert").delay(200).fadeOut(300);
 
-$(document).on('turbolinks:load', function (){
-	$(',rating').raty ( { path: '/assets', scoreName: 'comment[rating]' });
+	$(document).on('turbolinks:load', function (){
+		$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+		$('.rated').raty({ path: '/assets', 
+		readOnly: true,
+		score: function () {
+			return $(this).attr('data-score');
+		}
+	});
 });
