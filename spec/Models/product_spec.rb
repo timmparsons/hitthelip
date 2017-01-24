127d0604e420d98f222553c6e6b232f52be0c75e
Tim Parsons <tim.m.parsons@hotmail.com>
2017-01-24 13:12:48 -0800

    6.2 unit tests

---
+++ spec/Models/product_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Product do 
+	
+	before do
+	@product = Product.create!(name: "race bike")
+	@user = User.create!(email: "test@test.com", password: "password")
+		
+	@product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+	@product.comments.create!(rating: 3, user: @user, body: "Not bad")
+	@product.comments.create!(rating: 5, user: @user, body: "Love It!")
+
+	end
+
+	it "returns the average rating of all comments" do
+		expect(@product.average_rating).to eq "3"
+	end
+
+end
