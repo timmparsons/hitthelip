288c8db99154cbad29160054fea5fd76f2ab1b4c
Tim Parsons <tim.m.parsons@hotmail.com>
2017-01-23 17:41:51 -0800

    5.11 Consumer Web App

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
 			:to => 'your-email@example.com',
 			:subject => "A new contact form message from #{name}")
 	end
+
+	def welcome(user)
+		@appname = "Hit The Lip"
+		mail( :to => user.email,
+			  :subject => "Welcome to #{@appname}!")
+	end
 end
